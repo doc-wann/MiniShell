aecc45d7597db09ae19f0a2ef48cc6f9d3b5c732
Henrique Danielewicz <hdaniele@c2r7p5.42sp.org.br>
2023-07-24 15:47:56 -0300

    sending changes

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: hdaniele <hdaniele@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 10:19:37 by mmuriloj          #+#    #+#             */
-/*   Updated: 2023/07/17 18:04:23 by hdaniele         ###   ########.fr       */
+/*   Updated: 2023/07/18 17:53:29 by hdaniele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	ft_cd(char **args, char **envs)
 		else if (args[1])
 		{
 			if(chdir(ft_strjoin(homepath, path_parser(args[1]))) != 0)
-				write(2, ft_strjoin("cd: no such file or directory: ", args[1]), 31 + ft_strlen(args[1]));
+				write(1, ft_strjoin("cd: no such file or directory: ", args[1]), 31 + ft_strlen(args[1]));
 		}
 	}
 	else
